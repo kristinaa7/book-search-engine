2b6449a876863f6f4f063758ce790cffd9fb55dc
Kristina Alabastro <kristinaa7@live.com>
2023-04-19 13:19:13 -0700

    installed apollo client

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -9,6 +9,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
